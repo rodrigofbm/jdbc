0192978c366c86c56325f43e9e0f92b6349b2122
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-12-31 00:06:18 -0300

    show data

---
+++ src/db/DB.java
@@ -0,0 +1,70 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	private static Connection connection = null;
+	
+	public static Connection getConnection() {
+		if(connection == null) {
+			try {
+				Properties loadProps = loadProperties();
+				String dbUrl = loadProps.getProperty("dburl");
+				
+				connection = DriverManager.getConnection(dbUrl, loadProps);
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		
+		return connection;
+	}
+	
+	public static void closeConnection() {
+		if(connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
